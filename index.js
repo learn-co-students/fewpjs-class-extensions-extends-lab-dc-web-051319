47e2712f7a2050cd17e53c0c270d1760f912ba87
Jennifer Ingram <jennifer.ingram@flatironschool.com>
2019-06-28 14:28:07 -0400

    Wrote classes with inheritance

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Your code here
+class Polygon {
+    constructor(array) {
+        this.array = array
+    }
+
+    get countSides() {
+        return this.array.length
+    }
+
+    get perimeter() {
+        return this.array.reduce((acc, curVal) => acc + curVal, 0)
+    }
+}
+
+let rectangle = new Polygon([4, 5, 4, 5])
+console.log(rectangle.perimeter)
+
+class Triangle extends Polygon {
+    get isValid() {
+        let a = this.array
+        if (a[0] + a[1] > a[2] && a[1] + a[2] > a[0] && a[2] + a[0] > a[1]) {
+            return true
+        } else {
+            return false
+        }
+    }
+}
+
+let triangle = new Triangle([10, 5, 6])
+console.log(triangle.isValid)
+
+class Square extends Polygon {
+    get isValid() {
+        let a = this.array
+        if (a[0] === a[1] && a[1] === a[2] && a[2] === a[3]) {
+            return true
+        } else {
+            return false
+        }
+    }
+
+    get area() {
+        return this.array[0]**2
+    }
+}
