5ae8b477ee1bcaad43990ef8feb4f376021fb465
Matt Williams <matt.seongjun@gmail.com>
2019-06-28 11:13:47 -0400

    Working triangle test

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Your code here
+class Polygon {
+    constructor(sides){
+        this.sides = sides;
+        this.sortSides();
+    };
+
+    sortSides() {
+        this.sides.sort( (a, b) => {
+            if (Number.parseInt(a) < Number.parseInt(b)) {
+                return -1;
+            } else if (Number.parseInt(a) > Number.parseInt(b)) {
+                return 1;
+            } else {
+                return 0;
+            };
+        });
+    };
+
+    get countSides() {
+        return this.sides.length;
+    };
+
+    get perimeter() {
+        return this.sides.reduce( (side, perimeter) => side + perimeter );
+    };
+};
+
+class Triangle extends Polygon {
+    get isValid() {
+        if (this.countSides !== 3 ) {
+            return false;
+        } else {
+            return this.sides[2] < this.sides[1] + this.sides[0];
+        };
+    };
+};
